dfa239891f76a95834c27c538b223e2ef1aedaa9
vidhyashankar.m <vidhyashankar.m@phonepe.com>
2024-02-18 21:33:36 +0530

    lockandexecute test

--- src/main/java/io/appform/dropwizard/sharding/dao/operations/lockedcontext/LockAndExecute.java
+++ src/main/java/io/appform/dropwizard/sharding/dao/operations/lockedcontext/LockAndExecute.java
@@ -14,67 +14,71 @@ import lombok.NonNull;
 import org.hibernate.Session;
 
 /**
- * Acquires lock on an entity and perform all the operations given.
+ * Acquires lock on an entity and perform all the operations given. On INSERT mode, creates an
+ * entity and executes the given list of operations, all within a same DB txn. ON READ mode, gets an
+ * existing entity (the getter implementation should acquire a lock on this entity. This opcontext
+ * is not responsible for acquiring locks) and executes the given list of operations, all within a
+ * same DB txn.
  *
- * @param <T> Entity type on which lock is being acquired.
+ * @param <T> Entity type on which operations are to be executed.
  */
 @Data
 public class LockAndExecute<T> extends OpContext<T> {
 
-  private final List<Consumer<T>> operations = Lists.newArrayList();
-  @NonNull
-  private final Mode mode;
-  private Supplier<T> getter;
-  private Function<T, T> saver;
-  private T entity;
+    private final List<Consumer<T>> operations = Lists.newArrayList();
+    @NonNull
+    private final Mode mode;
+    private Supplier<T> getter;
+    private Function<T, T> saver;
+    private T entity;
 
-  @Builder(builderMethodName = "buildForRead", builderClassName = "ReadModeBuilder")
-  public LockAndExecute(@NonNull Supplier<T> getter) {
-    this.mode = Mode.READ;
-    this.getter = getter;
-  }
-
-  @Builder(builderMethodName = "buildForInsert", builderClassName = "InsertModeBuilder")
-  public LockAndExecute(@NonNull T entity, @NonNull Function<T, T> saver) {
-    this.mode = Mode.INSERT;
-    this.entity = entity;
-    this.saver = saver;
-  }
-
-  @Override
-  public T apply(Session session) {
-    T result = generateEntity();
-    operations
-        .forEach(operation -> operation.accept(result));
-    return result;
-  }
-
-  private T generateEntity() {
-    T result = null;
-    switch (mode) {
-      case READ:
-        result = getter.get();
-        if (result == null) {
-          throw new RuntimeException("Entity doesn't exist");
-        }
-        break;
-      case INSERT:
-        result = saver.apply(entity);
-        break;
-      default:
-        break;
-
+    @Builder(builderMethodName = "buildForRead", builderClassName = "ReadModeBuilder")
+    public LockAndExecute(@NonNull Supplier<T> getter) {
+        this.mode = Mode.READ;
+        this.getter = getter;
     }
-    return result;
-  }
 
-  @Override
-  public OpType getOpType() {
-    return OpType.LOCK_AND_EXECUTE;
-  }
+    @Builder(builderMethodName = "buildForInsert", builderClassName = "InsertModeBuilder")
+    public LockAndExecute(@NonNull T entity, @NonNull Function<T, T> saver) {
+        this.mode = Mode.INSERT;
+        this.entity = entity;
+        this.saver = saver;
+    }
 
-  @Override
-  public <R> R visit(OpContextVisitor<R> visitor) {
-    return visitor.visit(this);
-  }
+    @Override
+    public T apply(Session session) {
+        T result = generateEntity();
+        operations
+            .forEach(operation -> operation.accept(result));
+        return result;
+    }
+
+    private T generateEntity() {
+        T result = null;
+        switch (mode) {
+            case READ:
+                result = getter.get();
+                if (result == null) {
+                    throw new RuntimeException("Entity doesn't exist");
+                }
+                break;
+            case INSERT:
+                result = saver.apply(entity);
+                break;
+            default:
+                break;
+
+        }
+        return result;
+    }
+
+    @Override
+    public OpType getOpType() {
+        return OpType.LOCK_AND_EXECUTE;
+    }
+
+    @Override
+    public <R> R visit(OpContextVisitor<R> visitor) {
+        return visitor.visit(this);
+    }
 }
