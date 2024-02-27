aa08673a676723dfc4ce9b5582cb7b0efe0f9243
vidhyashankar.m <vidhyashankar.m@phonepe.com>
2024-02-27 12:13:40 +0530

    backward compatibility to spyglass db tracking bundle

--- src/main/java/io/appform/dropwizard/sharding/metrics/EntityOpMetricKey.java
+++ src/main/java/io/appform/dropwizard/sharding/metrics/EntityOpMetricKey.java
@@ -6,8 +6,18 @@ import lombok.Value;
 @Value
 @Builder
 public class EntityOpMetricKey {
+
     Class<?> entityClass;
     Class<?> daoClass;
     String commandName;
     String lockedContextMode;
+
+    /**
+     * @deprecated This is here for the backward compatibility to older version of spyglass tracing
+     * module. (field opType got renamed to commandName)
+     */
+    @Deprecated(forRemoval=true)
+    public String getOpType() {
+        return commandName;
+    }
 }
